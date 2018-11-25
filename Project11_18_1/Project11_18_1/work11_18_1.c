cace4e1047deeef25bc6b421fd3a4af29e714f4c
Arivan7586 <1194028288@qq.com>
2018-11-25 13:23:33 +0800

    操作符优化代码

--- Project11_18_1/Project11_18_1/work11_18_1.c
+++ Project11_18_1/Project11_18_1/work11_18_1.c
@@ -25,14 +25,54 @@ int getOne(int num)
 	return count;
 }
 
+int getOne1(unsigned int num)
+{
+	int count = 0;
+	int i = 0;
+	for (i = 0; i < 32; i++)  //需要移动32次
+	{
+		if ((num >> i) & 1)
+		{
+			count++;
+		}
+	}
+	return count;
+}
+
+int getOne2(int num)  //效率更好，不需要全部遍历
+{
+	int count = 0;
+	while (num)  //当num为0的时候，停止
+	{
+		if (num & 1)
+		{
+			count++;
+		}
+		num = num >> 1;
+	}
+	return count;
+}
+
+int getOne3(int num)   //遍历的次数更少
+{
+	int count = 0;
+	while (num) 
+	{
+		num = num & (num - 1);
+		count++;
+	}
+	return count;
+}
+
 int main()
 {
-	int ret = 0;
 	int input = 0;
 	printf("请输入一个数：");
 	scanf("%d",&input);
-	ret = getOne(input);
-	printf("%d\n",ret);
+	printf("%d\n", getOne(input));
+	printf("%d\n", getOne1(input));
+	printf("%d\n", getOne2(input));
+	printf("%d\n", getOne3(input));
 	system("pause");
 	return 0;
 }
